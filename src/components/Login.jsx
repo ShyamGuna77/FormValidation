f432d9abb722c52329ab39f5ef0c4a7dfcc9c5f6
Shyam <gshyamprasad2001@gmail.com>
2025-01-09 17:14:33 +0530

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,46 @@
+import { useState } from "react";
+
 export default function Login() {
+  const[email,setEmail] = useState('');
+  const[password,setPassword] = useState('');
 
   function handleSubmit(e){
     e.preventDefault()
     console.log("login");
+    console.log(email);
+    console.log(password);
+    
+  }
+  function handleEmail(e){
+     setEmail(e.target.value)
+ 
+     
+  }
+
+  function handlePAssword(e) {
+    setPassword(e.target.value);
+    
+    
   }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={handleEmail} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onClick={handlePAssword} />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button onClick={handleSubmit} className="button">Login</button>
+        <button onChange={handleSubmit} className="button">Login</button>
       </p>
     </form>
   );
